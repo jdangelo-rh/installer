a4ca5502a76933650dc13b4740153efb3b4fa318
Guillermo Ojea Quintana <gojeaqui@redhat.com>
2020-03-27 14:31:28 -0300

    Parent folder support

--- upi/vsphere/folder/main.tf
+++ upi/vsphere/folder/main.tf
@@ -1,11 +1,5 @@
-resource "vsphere_folder" "parent" {
-  path          = "${var.parent}"
-  type          = "vm"
-  datacenter_id = "${var.datacenter_id}"
-}
-
 resource "vsphere_folder" "folder" {
-  path          = "${vsphere_folder.parent.path}/${var.path}"
+  path          = "${var.parent}/${var.path}"
   type          = "vm"
   datacenter_id = "${var.datacenter_id}"
 }
--- upi/vsphere/resource_pool/main.tf
+++ upi/vsphere/resource_pool/main.tf
@@ -3,13 +3,16 @@ data "vsphere_compute_cluster" "compute_cluster" {
   datacenter_id = "${var.datacenter_id}"
 }
 
+/*
 resource "vsphere_resource_pool" "parent_resource_pool" {
   name                    = "${var.parent}"
   parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
 }
+*/
 
 resource "vsphere_resource_pool" "resource_pool" {
   name                    = "${var.name}"
-  parent_resource_pool_id = "${data.vsphere_resource_pool.parent_resource_pool.resource_pool_id}"
+//  parent_resource_pool_id = "${vsphere_resource_pool.parent_resource_pool.resource_pool_id}"
+  parent_resource_pool_id = "${var.parent}"
 }
 
